120d6aa54a286dac8351e10c65fabff850c0ec3e
wmvieira-philips <william.vieira@philips.com>
2023-07-22 16:33:21 -0300

    start spring boot

---
+++ src/test/java/com/spring/course/springcourse/SpringCourseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spring.course.springcourse;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringCourseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
